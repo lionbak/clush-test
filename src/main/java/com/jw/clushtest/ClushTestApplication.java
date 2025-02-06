390acfb1ca1ba62e4bee420816d1f602d13a8c60
Jinwoo Bak <jbak7469@gmail.com>
2025-02-06 19:09:49 +0900

    Initial commit

---
+++ src/main/java/com/jw/clushtest/ClushTestApplication.java
@@ -0,0 +1,13 @@
+package com.jw.clushtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClushTestApplication.class, args);
+    }
+
+}
